673b2ab8943823cc06e41b4142e2b3c067d7b140
André Luiz <andretechsouza@outlook.com>
2024-10-18 11:19:21 -0300

    Commit initial This is the initial commit of the project, containing configuration and execution files for this project

---
+++ eb-challenge/src/main/java/org/andretechsouza/ebanx/ebchallenge/EbChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.andretechsouza.ebanx.ebchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbChallengeApplication.class, args);
+    }
+
+}
---
+++ eb-challenge/src/test/java/org/andretechsouza/ebanx/ebchallenge/EbChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andretechsouza.ebanx.ebchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
